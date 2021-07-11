d41899ff2e52f3b43d843637a843b09cd50ad7d0
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-07-11 16:47:20 +0200

    added dotenv and fetch with query

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const fetch = require('node-fetch')
+
 
 app.use(express.static('public'))
 
+
+app.get('/data', async (req, res) => {
+    const query = `{ 
+        search(query: "stars:>50000"), type: REPOSITORY, first: 10) {
+            respositoryCount
+            edges {
+                node {
+                    ... on Respository {
+                        name
+                        owner {
+                            login
+                        }
+                        stargazers {
+                            totalCount
+                        }
+                    }
+                }
+            }
+        }
+    }`
+
+    const url = 'http://api.github.com/graphql'
+
+    const options = {
+        method: 'post',
+        headers: {
+            'content-type': 'application/json',
+            'authorization': 'bearer ' + process.env.APIKEY
+        },
+        body: JSON.stringify({ 'query': query }),
+    }
+
+    let response
+    try {
+        response = await fetch(url, options)
+    } catch (err) {
+        console.error(err)
+    }
+    const data = await response.json()
+
+    res.json(data)
+})
 
 app.listen(3000, () => console.log('Server Ready'))
